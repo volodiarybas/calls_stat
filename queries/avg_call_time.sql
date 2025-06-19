9fa6f2915f88a1ff5728eaaf6f9236109a60916a
Volodia <volodymyr.rybas.ext@aioneers.com>
2025-06-19 19:44:51 +0300

    Average outbound call duration query

--- queries/avg_call_time.sql
+++ queries/avg_call_time.sql
@@ -1,6 +1,10 @@
 -- ACERAGE CALL TIME;
 select avg(c.duration_seconds) as "Average Call Duration" from calls c;
 
+-- ACERAGE Outbound CALL TIME;
+select avg(c.duration_seconds) as "Average Outbound Call Duration" from calls c where c.direction = 'outbound';
+
+
 -- AVERAGE CALL TIME BY Agent
 select 
 	concat(a.first_name,' ', a.last_name)  as "Agent", 
