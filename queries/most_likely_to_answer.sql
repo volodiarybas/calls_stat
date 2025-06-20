5762c3d546486e2ca6c9a0ecb2f7ef89e2d7dec3
Volodia <volodymyr.rybas.ext@aioneers.com>
2025-06-20 16:33:16 +0300

    Queries optimization

--- queries/most_likely_to_answer.sql
+++ queries/most_likely_to_answer.sql
@@ -1,29 +1,47 @@
 -- Get Percentage of completed calls by hour of the day
-select 
-	extract(hour from c.call_time) as "Hour",
-	COUNT(*) AS "Total Calls",
-	COUNT(*) FILTER (WHERE status = 'completed') as "Completed Calls",
-	((COUNT(*) FILTER (WHERE status = 'completed'))::decimal / COUNT(*)) *100  as "Completed Calls %"
-from calls c 
-group by "Hour"
-order by "Completed Calls %" desc;
+WITH call_counts AS (
+ SELECT 
+ EXTRACT(HOUR FROM call_time) AS hour,
+ COUNT(*) AS total_calls,
+ COUNT(*) FILTER (WHERE status = 'completed') AS completed_calls
+ FROM calls
+ GROUP BY hour
+)
+SELECT 
+ hour,
+ total_calls,
+ completed_calls,
+ ROUND(
+ completed_calls::decimal / NULLIF(total_calls, 0) * 100, 2
+ ) AS completed_calls_percentage
+FROM call_counts
+ORDER BY completed_calls_percentage DESC;
 
 -- Get Percentage of completed calls by day of the week
-select 
-	extract(dow from c.call_time) as "Day of the week",
-	COUNT(*) AS "Total Calls",
-	COUNT(*) FILTER (WHERE status = 'completed') as "Completed Calls",
-	((COUNT(*) FILTER (WHERE status = 'completed'))::decimal / COUNT(*)) *100  as "Completed Calls %"
-from calls c 
-group by "Hour"
-order by "Completed Calls %" desc;
+WITH call_counts AS (
+ SELECT 
+ (EXTRACT(dow FROM call_time)) as day_of_the_week,
+ COUNT(*) AS total_calls,
+ COUNT(*) FILTER (WHERE status = 'completed') AS completed_calls
+ FROM calls
+ GROUP BY day_of_the_week
+)
+SELECT 
+		day_of_the_week,
+	 	total_calls,
+	 	completed_calls,
+	ROUND(
+ 		completed_calls::decimal / NULLIF(total_calls, 0) * 100, 2
+ 	) AS completed_calls_percentage
+FROM call_counts 
+ORDER BY completed_calls_percentage DESC;
 
 -- Get Percentage of by call type
 select
 	c.call_type,
 	COUNT(*) AS "Total Calls",
-	COUNT(*) FILTER (WHERE status = 'completed') as "Completed Calls",
-	((COUNT(*) FILTER (WHERE status = 'completed'))::decimal / COUNT(*)) *100  as "Completed Calls %"
+	COUNT(*) FILTER (WHERE status = 'completed') as completed_calls,
+	((COUNT(*) FILTER (WHERE status = 'completed'))::decimal / COUNT(*)) *100  as completed_calls_percentage
 from calls c 
 group by call_type
-order by "Completed Calls %" desc;
+order by completed_calls_percentage desc;
